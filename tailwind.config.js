bbb68200961a63145ac2e5a41db55fe3f54abf18
zepeersGily <>
2024-05-30 15:15:09 +0200

    add some custom options to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
    content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   ],
+   theme: {
+      extend: {
+         fontFamily: {
+            epilogue: ['Epilogue', 'sans-serif'],
+         },
+         boxShadow: {
+            secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+         },
+      },
+   },
+   plugins: [],
 }
-
