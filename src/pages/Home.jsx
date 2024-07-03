2390eed3ed69163ae60e39b6e90f615071cda47a
zepeersGily <>
2024-07-03 15:15:16 +0200

    implemented home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect, useContext } from 'react';
+import { web3Context } from '../context';
+import { DisplayCampaigns } from '../components';
+
 export default function Home() {
+
+   const [isLoading, setIsLoading] = useState(false);
+   const [campaigns, setCampaigns] = useState([]);
+
+   const { account, contract, getCampaigns } = useContext(web3Context);
+
+   useEffect(() => {
+      if (contract) {
+         (async function () {
+            setIsLoading(true);
+            const data = await getCampaigns();
+            setCampaigns(data);
+            setIsLoading(false);
+         })();
+      }
+   }, [account, contract]);
+
    return (
-      <div>Home</div>
+      <DisplayCampaigns
+         title='All Campaigns'
+         isLoading={isLoading}
+         campaigns={campaigns}
+      />
    )
 }
